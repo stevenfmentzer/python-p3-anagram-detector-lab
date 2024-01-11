b2868d0bf95e81c4fce50a57a1006d646f38fdec
Steven Mentzer <stevenfmentzer@gmail.com>
2024-01-10 21:30:51 -0500

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram: 
+    def __init__(self, word):
+
+        self.word = word 
+
+    def match(self, match_list):
+        main_list = sorted([letter for letter in self.word])
+        return_list = []
+        for word in match_list: 
+            match_list = sorted([letter for letter in word])
+            if main_list == match_list: 
+                return_list.append(word)
+        return return_list
